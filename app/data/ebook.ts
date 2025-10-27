8d00af9dff6be735e74a5f5788b8e082444efef7
unknown <brightstarmoxiz2@gmail.com>
2025-10-27 22:33:33 +0100

    another commit

--- app/data/ebook.ts
+++ app/data/ebook.ts
@@ -2,19 +2,19 @@ export const ebooks = [
   {
     id: 1,
     title: "Game & Earn",
-    price: 15.99,
-    image: "/nice.jpg",
+    price: 10000,
+    image: "/game.png",
   },
   {
     id: 2,
     title: "Book Before Bill",
-    price: 24.99,
-    image: "https://via.placeholder.com/300x200?text=Digital+Marketing+Mastery",
+    price: 2000,
+    image: "/bbb.png",
   },
   {
     id: 3,
     title: "Git Cheat Sheet",
-    price: 29.99,
-    image: "https://via.placeholder.com/300x200?text=AI+for+Beginners",
+    price: 1000,
+    image: "git.jpg",
   },
 ];
