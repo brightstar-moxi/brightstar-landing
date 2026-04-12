fe00d9bc98d17b878bf46d9d5cc4b6bce4d76c82
unknown <brightstarmoxiz2@gmail.com>
2026-04-12 06:24:36 +0100

    CV Builder4

--- app/cv-builder/[id]/page.tsx
+++ app/cv-builder/[id]/page.tsx
@@ -3,15 +3,17 @@
 import { useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 import { useParams } from "next/navigation";
-import CVBuilderPage from "../page";
+import CVBuilderClient from "../CVBuilderClient";
 
 export default function EditCVPage() {
   const params = useParams();
-  const id = params.id as string;
+  const id = params.id as any;
 
   const cv = useQuery(api.cv.getCV, { id });
 
   if (!cv) return <div>Loading...</div>;
 
-  return <CVBuilderPage initialData={cv.data} />;
+  if (cv === null) return <div>CV not found</div>;
+
+  return <CVBuilderClient initialData={cv.data} />;
 }
